cc06cdb4161f87eef490316a465484bc5d87ea99
Matheus Albino <matheusalbino@users.noreply.github.com>
2020-08-17 22:35:32 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.css'],
+  theme: {
+    extend: {
+      animation: {
+        'spin-20': 'spin 20s linear infinite',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
